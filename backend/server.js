0f2d71bce8fa6e63a5b46ac1536c49467d461ce6
Elisa Wardhani <elisaawardhani@gmail.com>
2025-12-17 20:54:10 +0700

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/uki');
+}
+
+main()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/api/employees', (req, res) => {
+    
   res.json({"note":"get all employees"})
 })
 
 app.post('/api/employees', (req, res) => {
+    console.log(req.body)
   res.json({"note":"Add employee"})
 })
 
