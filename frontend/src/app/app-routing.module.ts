ca1bf19c2d5885987703bce0da36bacf13df7daa
Elisa Wardhani <elisaawardhani@gmail.com>
2025-12-18 14:19:10 +0700

    feat: add pages (home, edit-employee) and layouts (header, footer) components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EditEmployeeComponent } from './pages/edit-employee/edit-employee.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'edit-employee', component: EditEmployeeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditEmployeeComponent } from './pages/edit-employee/edit-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    EditEmployeeComponent
   ],
   imports: [
     BrowserModule,
